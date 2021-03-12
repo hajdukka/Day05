00c97ef50c7c1d540e7a9105bace162de09034d4
hajdukka <nerminhajdo@gmail.com>
2021-03-12 21:41:31 +0100

    dodao ex02

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -3,7 +3,7 @@ function main() {
 
     var myLink;
 
-    var = '<a href= "http://www.example.com" traget= "_blank ">Link</a>';
+    myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 
     // Only change code above this line
