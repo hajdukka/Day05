ea10715f4ca4ce1ee27b74ea3d0802c4d49710fe
hajdukka <nerminhajdo@gmail.com>
2021-03-12 21:20:32 +0100

    dodao ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+
+    var myLink;
+
+    var = '<a href= "http://www.example.com" traget= "_blank ">Link</a>';
+
+
+    // Only change code above this line
+  
+    return myLink
+  }
+  
+  console.log(main());
+  module.exports = main;
