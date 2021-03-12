dfc70916ff26d3e85ec5f1d88b84f00452b51305
hajdukka <nerminhajdo@gmail.com>
2021-03-12 23:09:47 +0100

    dodao ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,22 @@
+function main() {
+  // Only change code below this line
+
+  var myNoun;
+  var myVerb;
+  var myAdjective;
+  var myAdverb;
+
+  myNoun = "dog";
+  myVerb = "ran";
+  myAdjective = "big";
+  myAdverb = "quickly";
+
+  var wordBlanks;
+  wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb + ".";
+
+  // Only change code above this line
+
+  return wordBlanks;
+}
+console.log(main());
+module.exports = main;
